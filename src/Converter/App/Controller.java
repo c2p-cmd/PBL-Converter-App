d1d93005fcf4128134380a910c1cbe919fd0b532
c2p-cmd <sharandt19@live.com>
2021-06-25 17:22:45 +0530

    Completion of Calculator Tab in TabPane

--- src/Converter/App/Controller.java
+++ src/Converter/App/Controller.java
@@ -1,14 +1,112 @@
 package Converter.App;
 
-//import javafx.fxml.FXML;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-//import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    // constants
+    static final Character ADD = '+';
+    static final Character SUBTRACT = '-';
+    static final Character PRODUCT = '*';
+    static final Character DIV = '/';
+    static final Character MODULO = '%';
+    static final Character POWER = '^';
+    static Character operatorChosen;
+
+    @FXML
+    public GridPane rootGridPane;
+
+    @FXML
+    public Tab calculatePane;
+
+    @FXML
+    public Tab convertPane;
+
+    @FXML
+    public TextField TextFieldOne;
+
+    @FXML
+    public TextField TextFieldTwo;
+
+    @FXML
+    public Button CalculateBtn;
+
+    @FXML
+    public Button ClearBtn;
+
+    @FXML
+    public ChoiceBox<Character> operatorBox;
+
+    @FXML
+    public TextField TextFieldRes;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) { }
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        operatorBox.getItems().addAll(ADD, SUBTRACT, PRODUCT, DIV, MODULO, POWER);
+
+        operatorBox.setValue(ADD);
+        operatorChosen = ADD;
+
+        operatorBox.getSelectionModel().selectedItemProperty().addListener(((observableValue, oldValue, newValue) -> {
+            //System.out.println("Old Value = " + oldValue + " New Value = " + newValue);
+            operatorChosen = newValue;
+        }));
+
+        CalculateBtn.setOnAction(actionEvent -> calculate());
+        ClearBtn.setOnAction(actionEvent -> clearFields());
+    }
+
+    public void clearFields() {
+        TextFieldOne.setText(null);
+        TextFieldTwo.setText(null);
+        TextFieldRes.setText(null);
+    }
+
+    public void calculate() {
+        Double operand1 = Double.parseDouble( TextFieldOne.getText() );
+        Double operand2 = Double.parseDouble( TextFieldTwo.getText() );
+        double result = 0.0;
+        switch (operatorChosen) {
+            case '+':
+                result = operand1+operand2;
+                break;
+            case '-':
+                result = operand1-operand2;
+                break;
+            case '*':
+                result = operand1*operand2;
+                break;
+            case '/':
+                if (operand2 == 0) {
+                    warnUser();
+                    result = 0.0;
+                } else {
+                    result = operand1/operand2;
+                }
+                break;
+            case '%':
+                result = operand1 % operand2;
+                break;
+            case '^':
+                result = Math.pow(operand1, operand2);
+                break;
+        }
+        TextFieldRes.setEditable(true);
+        TextFieldRes.setText( "Result = " + result );
+        TextFieldRes.setEditable(false);
+    }
+
+    private void warnUser() {
+        Alert warning = new Alert(Alert.AlertType.ERROR);
+        warning.setTitle("Error Occurred.");
+        warning.setHeaderText("Invalid Input");
+        warning.setContentText("Please Provide a valid input.");
+        warning.show();
+    }
 }
--- src/Converter/App/Main.java
+++ src/Converter/App/Main.java
@@ -1,35 +1,99 @@
 package Converter.App;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class Main extends Application {
 
-    private Controller controller;
+    Controller controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent fxmlLoader = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
-        GridPane rootNode = (GridPane) fxmlLoader;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        GridPane rootNode = fxmlLoader.load();
 
-        // TODO MenuBar
-        // TODO MenuPanes - New, About Developers, About App
+        controller = fxmlLoader.getController();
+
+        MenuBar menuBar = createMenu();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+
+        Pane menuPane = (Pane) rootNode.getChildren().get(0);
+        menuPane.getChildren().add(menuBar);
 
         Scene scene = new Scene(rootNode);
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Convert App");
+        primaryStage.setTitle("Converter App");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public MenuBar createMenu() {
+        // File Menu
+        Menu fileMenu = new Menu("File");
+        // File menu pane items
+        MenuItem resetEntry = new MenuItem("Reset Value");
+        resetEntry.setOnAction(actionEvent -> {
+            controller.clearFields();
+            // TODO more is there
+        });
+
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        MenuItem exitItem = new MenuItem("Exit App");
+        exitItem.setOnAction(actionEvent -> {
+            // exiting app
+            Platform.exit();
+            System.exit(0);
+        });
+
+        fileMenu.getItems().addAll(resetEntry, separatorMenuItem, exitItem);
+
+        // About
+        Menu helpMenu = new Menu("Help");
+        // about menu pane items
+        MenuItem aboutUs = new MenuItem("About Us");
+        aboutUs.setOnAction(actionEvent -> aboutUS());
+        MenuItem aboutApp = new MenuItem("About App");
+        aboutApp.setOnAction(actionEvent -> aboutApp());
+        // adding to help
+        helpMenu.getItems().addAll(aboutUs, new SeparatorMenuItem(), aboutApp);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+        return menuBar;
+    }
+
+    private void aboutUS() {
+        // showing AlertType.Information
+        Alert aboutDev = new Alert(Alert.AlertType.INFORMATION);
+        aboutDev.setTitle("About the Developers");
+        aboutDev.setHeaderText("PBL Batch-B1");
+        aboutDev.setContentText(
+                "We created this Converter App as our PBL project using JavaFX.\n"+
+                "Hope you all enjoy it!\n"
+        );
+        aboutDev.show();
+    }
+
+    private void aboutApp() {
+        // showing AlertType.Information
+        Alert aboutAppBox = new Alert(Alert.AlertType.INFORMATION);
+        aboutAppBox.setTitle("About Converter App");
+        aboutAppBox.setHeaderText("What is Converter?");
+        aboutAppBox.setContentText(
+                // TODO about section
+                "Brief up about Converter."
+        );
+        aboutAppBox.show();
     }
 }
