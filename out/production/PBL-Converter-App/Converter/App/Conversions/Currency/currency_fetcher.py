f46b0b2cc3eb2b81ea208988f66dee8681c2e2a4
c2p-cmd <sharandt19@live.com>
2021-08-01 13:47:25 +0530

    Removed CurrencyHandler.java & currency_fetcher.py

--- out/production/PBL-Converter-App/Converter/App/Conversions/Currency/currency_fetcher.py
+++
@@ -1,9 +0,0 @@
-import sys
-from py_currency_converter import convert
-
-currencyValue = int(sys.argv[1])
-fromCurrency = sys.argv[2]
-toCurrency = sys.argv[3]
-
-output = str(convert(base=fromCurrency, amount=currencyValue, to=[toCurrency,]))
-print(output[8:len(output)-1])
