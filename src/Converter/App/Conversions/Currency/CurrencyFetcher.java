1e43ce28a556a512de003cca862cb85132a308d7
c2p-cmd <sharandt19@live.com>
2021-08-01 13:46:03 +0530

    Removed CurrencyHandler.java & currency_fetcher.py

--- src/Converter/App/Conversions/Currency/CurrencyFetcher.java
+++ src/Converter/App/Conversions/Currency/CurrencyFetcher.java
@@ -7,7 +7,6 @@ import com.google.gson.JsonParser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
